e7afa839b1ed5b1a7190b8691eaa426712c6ed8a
Ishi Singh <210ishisingh@gmail/com>
2022-04-19 20:03:55 +0530

    Files Added

---
+++ sheep_count/Sheep_Count.ino
@@ -0,0 +1,32 @@
+int c=0;
+int p1=2;
+int p2=3;
+void setup()
+{
+  pinMode(p1,INPUT);
+  pinMode(p2,INPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  if(digitalRead(p1)==1)
+  {
+    delay(1000);
+    if(digitalRead(p2)==1)
+    {
+      c++;
+      Serial.println(c);
+    }
+    delay(2000);
+  }
+  if(digitalRead(p2)==1)
+  {
+    delay(1000);
+    if(digitalRead(p1)==1)
+    {
+      c--;
+      Serial.println(c);
+    }
+    delay(2000);
+  }
+}
